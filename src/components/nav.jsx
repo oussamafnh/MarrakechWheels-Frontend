846b11438817aad5e4923d015ac4b470c740ab48
oussamafnh <ofannah4@gmail.com>
2024-01-15 16:57:12 +0100

    update css

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,31 +3,55 @@ import "../style/nav.css";
 import { Darklogo } from '../asset/dark_logo';
 
 const Nav = () => {
-     return (
+    return (
         <div className="navbar">
             <div className="brand">
-                <a href="/" className='logo'>
+                <a href="/" className="logo">
                     <Darklogo />
                 </a>
             </div>
             <div className="menu">
                 <ul>
-                    <li> <a href="#" className="nav-link"> CARS </a> </li>
-                    <li> <a href="#" className="nav-link"> AGENCIES </a> </li>
-                    <li> <a href="#" className="nav-link"> ABOUT US </a> </li>
-                    <li> <a href="#" className="nav-link"> THE CITY </a> </li>
+                    <li>
+                        <a href="/" className="nav-link">
+                            CARS
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/" className="nav-link">
+                            AGENCIES
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/" className="nav-link">
+                            ABOUT US
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/" className="nav-link">
+                            THE CITY
+                        </a>
+                    </li>
                 </ul>
             </div>
             <div className="authentication">
                 <ul>
-                    <li><a href="#"> LOGIN </a></li>
-                    <li><a href="#"> SIGN UP </a></li>
+                    <li>
+                        <a href="/" className="nav-link">
+                            LOGIN
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/" className="nav-link">
+                            SIGN UP
+                        </a>
+                    </li>
                 </ul>
-
             </div>
         </div>
-    )
-}
+    );
+};
+
 
 
 
